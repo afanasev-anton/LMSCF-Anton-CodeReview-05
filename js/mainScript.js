9c78046f8fd48d5a318a2c3ddcfb329c5a987fb4
Anton Afanasev <afanasev.anton@icloud.com>
2020-02-16 17:09:22 +0100

    add sort function

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,8 +1,10 @@
-var items = movies;
-console.log(items);
-var itemsNumber = 3;
+
 
 $(document).ready(function(){
+	var items = movies;
+	console.log(items);
+	var itemsNumber = 3;
+	
 	showList (itemsNumber);
 	
 	var next = $(`<div class="row p-2"><button id="next" class="b-like btn btn-primary mx-auto">Show more</button></div>`)
@@ -11,6 +13,7 @@ $(document).ready(function(){
 		if (itemsNumber >= items.length) {
 			itemsNumber = 3;
 			showList (itemsNumber);
+			$(`#next`).show();
 		} else {			
 			if ((items.length-itemsNumber) > 3) {
 				itemsNumber += 3;
@@ -18,6 +21,7 @@ $(document).ready(function(){
 			} else {
 				itemsNumber += (items.length-itemsNumber);
 				showList (itemsNumber);
+				$(`#next`).hide();
 			}
 		}
 	})
@@ -26,6 +30,7 @@ $(document).ready(function(){
 	$("#b-items").click(function(){
 		$("#list").empty();
 		showList(3);
+		$(`#next`).show();
 	});
 
 	//place movies-cards in container #content	
@@ -39,7 +44,7 @@ $(document).ready(function(){
 							<div class="card bg-dark text-white">
 								<img class="card-img-top" src="${items[i].thumb}" alt="poster">
 								<div class="card-body">
-									<h4 class="card-title">${items[i].name}</h4>
+									<h4 class="card-title">${items[i].name}<span class="small"> [${items[i].year}]</span></h4>
 									<p class="card-text">${items[i].description}</p>
 									<button id="b-${items[i].iD}" class="b-like btn btn-primary">Like</button>
 									<span class="text-warning font-weight-bold">${items[i].likes}&#9733;</span>
@@ -53,32 +58,31 @@ $(document).ready(function(){
 				$(this).next().empty();
 				$(this).next().append(`${items[i].likes}&#9733;`);
 				
-				console.log(items[i].likes);
+				//console.log(items[i].likes);
 			});
 		}
 
-		$("#list").show(600);//shows thumbnails with delay
+		$("#list").show(400);//shows thumbnails with delay
 	}
 	// sort button
 	$("#sort").click(function(){
 	//sort function
-		
 		for (var i = 0; i < items.length; i++) {
-			var k = i;
-			var min = items[i];
-			for (var j = i+1; i < items.length; i++) {
-				if (items[j].likes < min) {
-					min = items[j];
-					k = j;
-				}
-			}
-
+			var max = i;
 			
+			var piv = items[i];
+			
+			for (var j = i; j < items.length; j++) {
+				if (items[j].likes > items[max].likes) {max = j;}
+			}
+			items[i] = items[max];
+			items[max] = piv;						
 		}
+		showList(3);
 	//end of sort func
 	});
 
 	
 
-//do not delete the line after these comment
+//do not delete line after these comment
 });
