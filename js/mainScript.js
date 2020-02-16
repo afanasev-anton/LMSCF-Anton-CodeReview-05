52b62f0ea7d00386a96469896d9a18e84ba217a5
Anton Afanasev <afanasev.anton@icloud.com>
2020-02-16 21:12:53 +0100

    add all functions

--- js/mainScript.js
+++ js/mainScript.js
@@ -5,22 +5,25 @@ $(document).ready(function(){
 	console.log(items);
 	var itemsNumber = 3;
 	
-	showList (itemsNumber);
+	showList (0,itemsNumber);
 	
+	//button "Show more"
 	var next = $(`<div class="row p-2"><button id="next" class="b-like btn btn-primary mx-auto">Show more</button></div>`)
 	$("#content").append(next);
 	next.click(function(){		
 		if (itemsNumber >= items.length) {
 			itemsNumber = 3;
-			showList (itemsNumber);
+			showList (1,itemsNumber);
 			$(`#next`).show();
 		} else {			
 			if ((items.length-itemsNumber) > 3) {
+				var k=itemsNumber;
 				itemsNumber += 3;
-				showList (itemsNumber);
+				showList (k,itemsNumber);
 			} else {
+				var k=itemsNumber;
 				itemsNumber += (items.length-itemsNumber);
-				showList (itemsNumber);
+				showList (k,itemsNumber);
 				$(`#next`).hide();
 			}
 		}
@@ -28,42 +31,84 @@ $(document).ready(function(){
 	
 	//nav bar
 	$("#b-items").click(function(){
+		items = movies;
 		$("#list").empty();
-		showList(3);
+		itemsNumber = 3;
+		showList(0,itemsNumber);
+		$(`#next`).show();
+	});
+	$("#action").click(function(){
+		items = [];
+		var k=0;
+		for (var i = 0; i < movies.length; i++) {
+			if (movies[i].genre == "action") {
+				items[k]=movies[i];
+				k++;
+			}
+		}
+		$("#list").empty();
+		itemsNumber = items.length;
+		showList(0,itemsNumber);
+		$(`#next`).show();
+	});
+	$("#comedy").click(function(){
+		items = [];
+		var k=0;
+		for (var i = 0; i < movies.length; i++) {
+			if (movies[i].genre == "comedy") {
+				items[k]=movies[i];
+				k++;
+			}
+		}
+		$("#list").empty();
+		itemsNumber = items.length;
+		showList(0,itemsNumber);
 		$(`#next`).show();
 	});
 
 	//place movies-cards in container #content	
-	function showList (num) {
-		$("#list").empty();
-		$("#list").hide();//hide to show again with animation
+	function showList (start,end) {
 
-		for (let i = 0; i < num; i++) {
+		for (let i = start; i < end; i++) {
 			
-			var card = $(`<div class="col-sm-12 col-md-6 col-lg-4 p-2">
+			var card = $(`<div class="col-12 col-sm-6 col-md-4 p-2">
 							<div class="card bg-dark text-white">
 								<img class="card-img-top" src="${items[i].thumb}" alt="poster">
 								<div class="card-body">
 									<h4 class="card-title">${items[i].name}<span class="small"> [${items[i].year}]</span></h4>
 									<p class="card-text">${items[i].description}</p>
-									<button id="b-${items[i].iD}" class="b-like btn btn-primary">Like</button>
-									<span class="text-warning font-weight-bold">${items[i].likes}&#9733;</span>
+									<div class="row"><button id="b-${items[i].iD}" class="b-like btn btn-primary">Like</button>
+									<div class="w-75"><h4 class="text-warning font-weight-bold text-right"></h4></span></div>
 								</div>
 							</div>
 						</div>`);
 			$(`#list`).append(card);
-			
+			//place stars next to button "like"
+			if (items[i].likes == 0) {
+				$(`#b-${items[i].iD}`).next().children().append(`0`);
+			} else {
+				if (items[i].likes < 6) {
+					for (var l = 0; l < items[i].likes; l++) {
+						$(`#b-${items[i].iD}`).next().children().append(` &#9733;`);
+					}
+				} else {$(`#b-${items[i].iD}`).next().children().append(`${items[i].likes}&#9733;`);}				
+			}			
+
+			// function for the button "like", add stars then pushing the button
 			$(`#b-${items[i].iD}`).click(function(){
 				items[i].likes++;
-				$(this).next().empty();
-				$(this).next().append(`${items[i].likes}&#9733;`);
-				
-				//console.log(items[i].likes);
+				$(this).next().children().empty();
+				if (items[i].likes < 6) {
+					for (var l = 0; l < items[i].likes; l++) {
+						$(this).next().children().append(` &#9733;`);
+					}
+				} else {$(this).next().children().append(`${items[i].likes}&#9733;`);}
+				//$(this).next().append(` ${items[i].likes}&#9733;`);
 			});
+			//end of the "like" function
 		}
-
-		$("#list").show(400);//shows thumbnails with delay
 	}
+	
 	// sort button
 	$("#sort").click(function(){
 	//sort function
@@ -78,7 +123,8 @@ $(document).ready(function(){
 			items[i] = items[max];
 			items[max] = piv;						
 		}
-		showList(3);
+		$("#list").empty();
+		showList(0,itemsNumber);
 	//end of sort func
 	});
 
